24264f9001a2aeaadaa70a75ee78db1dae80b62b
Ben-Limo <bensokiplimo@gmail.com>
2024-07-12 14:41:12 +0300

    add smooth scrolling on mouse click

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import 'bootstrap'; // bootstrap js
 
 import './css/styles.css'; // bootstrap css and custom css
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+});
