2fa67e91be20a6496a229cb8627a20fbc233d377
Ben-Limo <bensokiplimo@gmail.com>
2024-07-12 15:16:14 +0300

    add what we do toggle func

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import 'bootstrap'; // bootstrap js
+import $ from 'jquery';
 
 import './css/styles.css'; // bootstrap css and custom css
 
@@ -11,3 +12,21 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     });
   });
 });
+
+$(function () {
+  $('.what-we-do-title').addClass('fw-bold text-center');
+  toggleWhatWeDo();
+});
+
+//  function to toggle what we do
+function toggleWhatWeDo() {
+  $('.service').on('click', function () {
+    if ($('.service-details', this).is(':visible')) {
+      $('.service-details', this).hide();
+      $('.service-card', this).fadeIn('slow');
+    } else {
+      $('.service-details', this).fadeIn('slow');
+      $('.service-card', this).hide();
+    }
+  });
+}
